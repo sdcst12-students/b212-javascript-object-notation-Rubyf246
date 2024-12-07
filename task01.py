1ff6f0f79c5b16134df9573d7411a6c1384f0055
Rubyf246 <144410608+Rubyf246@users.noreply.github.com>
2024-12-07 12:21:42 -0800

    done task 1 and 2

--- task01.py
+++ task01.py
@@ -1,9 +1,34 @@
 #!python3
 # Read the contents of a file that has a json encoded list of numbers.
 # Find the largest number in each list
-# task01a: 63545
-# task01b: 63876
-# task01c: 63891
+# task02a: 63545
+# task02b: 63876
+# task02c: 63892
 
 import json
 
+def find_sum():
+    datatask2 = 'task01a.txt'
+    data2 = open(datatask2,'r')
+    str2 = data2.read()
+    decoded2 = json.loads(str2)
+    #print(f"decodedNumbers is a variable of type {type(decoded2)} and contains {decoded2}")
+    print(max(decoded2))
+    
+    datatask2 = 'task01b.txt'
+    data2 = open(datatask2,'r')
+    str2 = data2.read()
+    decoded2 = json.loads(str2)
+    #print(f"decodedNumbers is a variable of type {type(decoded2)} and contains {decoded2}")
+    print(max(decoded2))
+    
+    datatask3 = 'task01c.txt'
+    data3 = open(datatask3,'r')
+    str3 = data3.read()
+    decoded3 = json.loads(str3)
+    #print(f"decodedNumbers is a variable of type {type(decoded2)} and contains {decoded2}")
+    print(max(decoded3))
+    
+
+if __name__ == "__main__":
+    find_sum()    
--- task02.py
+++ task02.py
@@ -6,9 +6,17 @@ import json
 # there are different methods that we can retrieve the data with: POST and GET
 # You can google the difference between POST and GET requests
 
-req = requests.get('http://sdcaf.hungrybeagle.com/menu.php')
-data = req.text
+req = requests.get('http://sdcaf.hungrybeagle.com/menu.php') # go to internet to get info
+data = req.text #data is str 
 
 # Use the json encoded data that is retrieved from this website and print out the weekly menu
 # You will need to decipher the json decoded data to determine what information the 
 # dictionary object contains
+
+decoded = json.loads(data) # decoded is a dictionary
+print(f"decoded is a variable of type {type(decoded)} and contains {decoded}")
+
+print ("----------------------------------")
+json_formatted_str = json.dumps(decoded, indent=4) # convert json(essentially a dictionary) into a string
+print(json_formatted_str)
+ 
